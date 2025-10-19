aa5a616cf6200229dcd5d3ea820ca459f13b912f
poketopa <lhs5427ll@gmail.com>
2025-10-19 18:51:49 +0900

    feat(service): 커스텀 구분자 검증 메서드 추가

--- src/test/java/calculator/service/CalculatorService.java
+++ src/test/java/calculator/service/CalculatorService.java
@@ -1,5 +1,21 @@
 package calculator.service;
 
 public class CalculatorService {
+    public String checkDelimiter(String input){
+        if(isDelimiter(input)){
+            return String.valueOf(input.charAt(2));
+        }
+        return null;
+    }
 
+    public boolean isDelimiter(String input){
+        if(input.length() >= 5
+        && input.charAt(0) == '/'
+        && input.charAt(1) == '/'
+        && input.charAt(3) == '\\'
+        && input.charAt(4) == 'n'){
+            return true;
+        }
+        return false;
+    }
 }
