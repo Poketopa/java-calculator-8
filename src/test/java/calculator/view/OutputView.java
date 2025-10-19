fc86ab4ecf9d1d7b2868f9ea5336573a45f32d0d
poketopa <lhs5427ll@gmail.com>
2025-10-19 18:22:42 +0900

    feat(view): 결과 출력을 위한 OutputView 추가

---
+++ src/test/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import java.math.BigInteger;
+
+public class OutputView {
+    public void printResult(BigInteger result){
+        System.out.print("결과 : " + result);
+    }
+}
